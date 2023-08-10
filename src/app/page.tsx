b86ce4356198e9f4139c44e0b02b995e52ec69c1
ClaytonAstrom <castromxiii@gmail.com>
2023-08-09 22:15:22 -0500

    Initital setup (#1)

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+function MyButton() {
+  return (
+    <button>I&apos;m a button</button>
+  );
+}
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my testTESTA fast reload</h1>
+      <MyButton />
+    </div>
+  )
+}
