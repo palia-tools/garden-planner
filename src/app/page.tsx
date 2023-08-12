dfd9c64ab1dc5baf304e9cb464032f42cbdc7391
Clayton Astrom <castromxiii@gmail.com>
2023-08-12 16:15:30 +0000

    Properties passing example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,106 @@
-import Image from 'next/image'
+'use client';
+import Button from '@mui/material/Button'
+import { createTheme, styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import React from 'react';
+import ToggleButton from '@mui/material/ToggleButton';
 
 function MyButton() {
   return (
-    <button>I&apos;m a button</button>
+    <Button variant="contained">I&apos;m a button</Button>
   );
 }
 
-export default function Home() {
+function MyGrid({color}: {color: string}) {
+  let boxes = [];
+
+  function handleMouseOver(e: React.MouseEvent<HTMLDivElement>) {
+    e.currentTarget.style.backgroundColor = color;
+  }
+
+  function handleMouseOut(e: React.MouseEvent<HTMLDivElement>) {
+    e.currentTarget.style.backgroundColor = 'white';
+  }
+
+  for (let i = 0; i < 4; i++) {
+    boxes.push(
+      <Grid key={i} item xs={1}>
+        <Item onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} >xs=8</Item>
+      </Grid>
+    )
+  }
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        {boxes}
+      </Grid>
+    </Box>
+  );
+}
+
+type PickerProps = {
+  color: string;
+  onChange: (event: React.MouseEvent<HTMLElement>, newColor: string) => void;
+}
+
+function Picker({color, onChange}: PickerProps) {
+  
+
+  return (
+      <ToggleButtonGroup
+        color="primary"
+        value={color}
+        exclusive
+        onChange={onChange}
+        aria-label="Color"
+      >
+        <ToggleButton value="red" sx={{color: "white"}}>Red</ToggleButton>
+        <ToggleButton value="green" sx={{color: "white"}}>Green</ToggleButton>
+        <ToggleButton value="blue" sx={{color: "white"}}>Blue</ToggleButton>
+      </ToggleButtonGroup>
+  )
+}
+
+function EditArea() {
+  const [color, setColor] = React.useState('');
+
+  const handleChange = (
+    e: React.MouseEvent<HTMLElement>,
+    newColor: string,
+  ) => {
+    setColor(newColor);
+  }
+
   return (
     <div>
-      <h1>Welcome to my testTESTA fast reload</h1>
-      <MyButton />
+      <MyGrid color={color} />
+      <Picker color={color} onChange={handleChange} />
     </div>
+  )
+}
+
+function PageObject() {
+  return (
+    <div>
+      <EditArea />
+    </div>
+  )
+}
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function Home() {
+  return (
+    <PageObject />
   )
 }
